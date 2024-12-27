6aa3f788d52cceaf5bd02f01d028d946ee8136d8
Jiuru-Kun <1360223184@qq.com>
2024-12-28 01:15:24 +0800

    Update settings.lua to 1.2.0

--- settings.lua
+++ settings.lua
@@ -1,4 +1,4 @@
--- SIO II Clock Widget: Horizon, Version 1.1.0
+-- SIO II Clock Widget: Horizon, Version 1.2.0
 local settings
 local function getIndex(list, target)
 	for i = 1, #list do
@@ -12,14 +12,24 @@ local lineStyles = {"Green(Default)[zh]绿色(默认)", "Orange[zh]橙色", "Gre
 
 function script:init()
     settings = Util.optStorage(TheoTown.getStorage(), self:getDraft():getId()..':settings')
+    settings.note = TheoTown.translateInline("Changes will be applied after reentering a city.[zh]设置将会在重进城市后生效")
+    settings.lineStyle = settings.lineStyle or "Green(Default)"
+    
     if settings.alwaysShowRealTime == nil then
         settings.alwaysShowRealTime = false
     else
         settings.alwaysShowRealTime = settings.alwaysShowRealTime
     end
-    settings.note = TheoTown.translateInline("Changes will be applied after reentering a city.[zh]设置将会在重进城市后生效")
-    settings.lineStyle = settings.lineStyle or "Green(Default)"
-    
+    if settings.showDate == nil then
+        settings.showDate = false
+    else
+        settings.showDate = settings.showDate
+    end
+     if settings.showSeconds == nil then
+        settings.showSeconds = false
+    else
+        settings.showSeconds = settings.showSeconds
+    end
     -- Address the translation
     for i = 1, #lineStyles do
         lineStyles[i] = TheoTown.translateInline(lineStyles[i])
@@ -47,12 +57,30 @@ function script:settings()
 	        end
         },
         {
-            name = TheoTown.translateInline("Always show the real time(May needs high performance requirements)[zh]显示现实时间(可能有较高的性能要求)"),
+            name = TheoTown.translateInline("Always show the real time(SRT)[zh]显示现实时间"),
             value = settings.alwaysShowRealTime,
             onChange = function(newState) 
                 settings.alwaysShowRealTime = newState 
                 local condition = newState and 1 or 0
                 TheoTown.setGlobalFunVar("!Sio2GlobalAlwaysRealTime", condition)
+            end
+        },
+        {
+            name = TheoTown.translateInline("Show seconds(Requires SRT is enable)[zh]显示秒数(需要启用显示现实时间)"),
+            value = settings.showSeconds,
+            onChange = function(newState) 
+                settings.showSeconds = newState 
+                local condition = newState and 1 or 0
+                TheoTown.setGlobalFunVar("!Sio2GlobalShowSeconds", condition)
+            end
+        },
+        {
+            name = TheoTown.translateInline("Show the date[zh]显示日期"),
+            value = settings.showDate,
+            onChange = function(newState) 
+                settings.showDate = newState 
+                local condition = newState and 1 or 0
+                TheoTown.setGlobalFunVar("!Sio2GlobalShowDate", condition)
             end
         }
     }
